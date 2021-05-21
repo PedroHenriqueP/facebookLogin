dc84e053d211d9143bc01d0d7ef054693520be87
PedroHenriqueP <pedrohenriquedasilv@gmail.com>
2021-05-21 18:08:45 -0300

    form

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import { Section, Body, Options, Accounts, CardProfile, Profile, Form } from './styled';
+import {
+  Section,
+  Body,
+  Options,
+  Accounts,
+  CardProfile,
+  Profile,
+  AddAccount,
+  LimitProfile,
+  Form,
+  Itens,
+  Login,
+  Password,
+  ButtonLogin,
+  Line
+} from "./styled";
 
-import { FiPlusCircle } from 'react-icons/fi';
+import { FiPlusCircle } from "react-icons/fi";
 
-import Perfil from './assets/perfil.jpeg';
+import Perfil from "./assets/perfil.jpeg";
+import Add from "./assets/add.png";
 
 function App() {
   return (
@@ -10,22 +26,33 @@ function App() {
       <Body>
         <Options>
           <h2>facebook</h2>
-          <a className = 'access'>Acessos recentes</a>
-          <a className = 'click'>Clica na tua foto ou adiciona uma conta.</a>
+          <a className="access">Acessos recentes</a>
+          <a className="click">Clica na tua foto ou adiciona uma conta.</a>
           <Accounts>
             <CardProfile>
-              <Profile src={Perfil}/>
+              <Profile src={Perfil} />
               <a>Pedro</a>
             </CardProfile>
 
             <CardProfile>
-              <FiPlusCircle/>
-              <a>Adicionar Conta</a>
+              <LimitProfile>
+                <AddAccount src={Add} />
+              </LimitProfile>
+              <a className = 'add'>Adicionar Conta</a>
             </CardProfile>
           </Accounts>
         </Options>
-        <Form>
 
+        <Form>
+          <Itens>
+            <Login placeholder = 'E-mail ou número de telemóvel' type = 'e-mail' required/>
+            <Password placeholder = 'Palavra-passe' type = 'password'/>
+            <ButtonLogin>Iniciar sessão</ButtonLogin>
+            <a href = '#'>Não sabes tua palavra-passe?</a>
+            <Line/>
+            <a className = 'create'>Criar nova conta</a>
+          </Itens>
+          <a>Criar Uma Página para uma celebridade, banda ou empresa.</a>
         </Form>
       </Body>
     </Section>
--- src/styled.js
+++ src/styled.js
@@ -3,12 +3,14 @@ import styled from 'styled-components';
 export const Section = styled.section`
     background-color: #f0f2f5;
     width: 100%;
+    height: 100vh;
 `;
 
 export const Body = styled.div`
     padding: 100px 0;
     display: flex;
     justify-content: space-around;
+    align-items: center;
 `;
 
 export const Options = styled.div`
@@ -43,6 +45,24 @@ export const Profile = styled.img`
     border-top-right-radius: 10px;
 `;
 
+export const AddAccount = styled.img`
+    width: 40px;
+    height: 40px;
+    border-top-left-radius: 10px;
+    border-top-right-radius: 10px;
+    margin: auto;
+`;
+
+export const LimitProfile = styled.div`
+    width: 160px;
+    height: 160px;
+    display: flex;
+    align-items: center;
+    background-color: #f0f2f5;
+    border-top-left-radius: 10px;
+    border-top-right-radius: 10px;
+`;
+
 export const CardProfile = styled.div`
     display: flex;
     align-itens: center;
@@ -52,9 +72,18 @@ export const CardProfile = styled.div`
     width: 160px;
     border-radius: 10px;
     margin: 0 10px;
+    box-shadow: 0 0 .1em #898989;
     transition: .4s;
+    cursor: pointer;
     >a{
         padding: 10px 0;
+        font-family: SFProText-Medium, Helvetica, Arial, sans-serif;
+        color: #4b4f56;
+    }
+    >.add{
+        background-color: #f0f2f5;
+        border-bottom-left-radius: 10px;
+        border-bottom-right-radius: 10px;
     }
     &:hover{
         box-shadow: 0 0 .4em black;
@@ -62,5 +91,80 @@ export const CardProfile = styled.div`
 `;
 
 export const Form = styled.div`
+    background-color: #fff;
+    box-shadow: 0 0 .8em #dddfe2;
+    border-radius: 10px;
+    height: 340px;
+    display: flex;
+    flex-direction: column;
+    >a{
+        color: #1c1e21;
+        font-family: SFProText-Regular, Helvetica, Arial, sans-serif;
+        font-size: 14px;
+        text-align: center;
+    }
+`;
 
+export const Itens = styled.div`
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    >a{
+        text-align: center;
+        margin: 10px 0;
+        color: #1877f2;
+        font-size: 15px;
+        text-decoration: none;
+    }
+    >.create{
+        background-color: #5db82c;
+        color: #fff;
+        width: 190px;
+        border-radius: 6px;
+        font-size: 20px;
+        line-height: 48px;
+        font-family: inherit;
+        margin: 20px auto;
+        cursor: pointer;
+    }
+    >hr{
+	    border-top: 10px solid #094CFA;
+    }
+`;
+
+export const Line = styled.div`
+    border-top: 1px solid #dddfe2;
+`;
+
+export const Login = styled.input`
+    margin-bottom: 20px;
+    font-family: inherit;
+    border-radius: 6px;
+    font-size: 17px;
+    padding: 14px 16px;
+    width: 330px;
+    border: 1px solid #dddfe2;
+    color: #1d2129;
+`;
+
+export const Password = styled.input`
+    margin-bottom: 20px;
+    font-family: inherit;
+    border-radius: 6px;
+    font-size: 17px;
+    padding: 14px 16px;
+    width: 330px;
+    border: 1px solid #dddfe2;
+    color: #1d2129;
+`;
+
+export const ButtonLogin = styled.button`
+    background-color: #1877f2;
+    border: none;
+    border-radius: 6px;
+    font-size: 20px;
+    line-height: 48px;
+    color: #fff;
+    font-family: inherit;
+    cursor: pointer;
 `;
